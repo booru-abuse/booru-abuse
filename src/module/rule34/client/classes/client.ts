01b4485cd2f31893f0d55fb02d346f832779c1a9
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-03-03 18:35:38 -0600

    feat: add apiurl to client

--- src/module/rule34/client/classes/client.ts
+++ src/module/rule34/client/classes/client.ts
@@ -4,21 +4,29 @@ import { BooruAbuseError } from "../../../../error/classes/booru-abuse-error.ts"
 import { APIURL } from "../../api/url/functions/api-url.ts";
 import type { Authentication } from "../interfaces/authentication.ts";
 import type { ClientOptions } from "../interfaces/client-options.ts";
+import type { APIURLParameterMap } from "../../api/url/interfaces/api-parameter-map.ts";
 
 /** Client to retrieve data from Rule 34 at rule34.xxx. */
 export class Client {
     #auth: Authentication;
 
+    protected client = this;
     protected authorized: boolean = false;
     
     /** The user tied to the client. */
     self: ClientUser;
+
+    APIURL = <S extends keyof APIURLParameterMap>(
+        s: S,
+        params: Omit<APIURLParameterMap[S]["params"], keyof Authentication>,
+        ...args: APIURLParameterMap[S]["args"]
+    ) => APIURL(s, { ...params, ...this.#auth }, ...args);
     
     constructor (options: ClientOptions) {
         this.#auth = options.auth;
         this.self = ClientUser.fromAuth(options.auth);
     }
-    
+
     /**
      * Returns the client if the credentials are valid, otherwise throws an
      * error.
